7e5ee8f9897da233e394a67c63a3372365069cb7
roshansingh2444 <48400264+roshansingh2444@users.noreply.github.com>
2023-01-11 21:12:10 +0530

    added brush thickness

--- app/src/main/java/com/d/drawapp/DrawingView.kt
+++ app/src/main/java/com/d/drawapp/DrawingView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -31,7 +32,7 @@ class DrawingView(context: Context,attrs: AttributeSet) : View(context,attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+      //  mBrushSize = 20.toFloat()
 
     }
 
@@ -89,6 +90,12 @@ class DrawingView(context: Context,attrs: AttributeSet) : View(context,attrs) {
         }
         invalidate()
         return true
+    }
+    fun setSizeForBrush(newSize : Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        newSize,resources.displayMetrics
+            )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 }
