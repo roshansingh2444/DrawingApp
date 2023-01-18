9a206cbbf25a763438ce7f562c382feeb3f1a661
roshansingh2444 <48400264+roshansingh2444@users.noreply.github.com>
2023-01-18 14:21:21 +0530

    added button thickness dialogbox

--- app/src/main/java/com/d/drawapp/MainActivity.kt
+++ app/src/main/java/com/d/drawapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.d.drawapp
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size :")
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBtn.setOnClickListener(View.OnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        })
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener(View.OnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        })
+
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        largeBtn.setOnClickListener(View.OnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        })
+        brushDialog.show()
     }
 }
